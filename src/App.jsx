f4750cd541e95c0b8ac2a1174345059797e429b8
Gustavo Medeiros <oigostavo@gmail.com>
2025-06-16 17:28:28 -0300

    feat: implementação do tailwind e configuração do proprio

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
+      <div className="app">
+        <div className="background-container">
+          <img src="/src/assets/images/Background-Illustration.gif" alt="Background" className="background-image" />
+        </div>
+        
+        <Header />
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
           {/* Add more routes here as needed */}
-        </Route>
-      </Routes>
+        </Routes>
+        
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Add VLibras widget
+const script = document.createElement('script');
+script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js';
+script.async = true;
+script.onload = () => {
+  new window.VLibras.Widget('https://vlibras.gov.br/app');
+};
+document.head.appendChild(script);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
